909fc3937a66d2125aff3f1f5d75454a93df94fe
Tatarincev <et@virtoway.com>
2016-03-01 15:19:24 +0200

    2.8 version changes

--- Altsoft.ShopifyImportModule/Altsoft.ShopifyImportModule.Web/Controllers/Api/ShopifyImportController.cs
+++ Altsoft.ShopifyImportModule/Altsoft.ShopifyImportModule.Web/Controllers/Api/ShopifyImportController.cs
@@ -7,6 +7,7 @@ using Hangfire;
 using VirtoCommerce.Domain.Store.Services;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.PushNotifications;
+using VirtoCommerce.Platform.Core.Security;
 
 namespace Altsoft.ShopifyImportModule.Web.Controllers.Api
 {
@@ -16,11 +17,14 @@ namespace Altsoft.ShopifyImportModule.Web.Controllers.Api
         private readonly IPushNotificationManager _notifier;
         private readonly IShopifyImportService _shopifyImportService;
         private readonly IStoreService _storeService;
-        public ShopifyImportController(IPushNotificationManager notifier, IShopifyImportService shopifyImportService, IStoreService storeService)
+        private readonly IUserNameResolver _userNameResolver;
+        public ShopifyImportController(IPushNotificationManager notifier, IShopifyImportService shopifyImportService,
+                                       IStoreService storeService, IUserNameResolver userNameResolver)
         {
             _notifier = notifier;
             _shopifyImportService = shopifyImportService;
             _storeService = storeService;
+            _userNameResolver = userNameResolver;
         }
 
         [HttpPost]
@@ -28,7 +32,7 @@ namespace Altsoft.ShopifyImportModule.Web.Controllers.Api
         [Route("start-import")]
         public IHttpActionResult StartImport(ShopifyImportParams importParams)
         {
-            var notification = new ShopifyImportNotification(CurrentPrincipal.GetCurrentUserName())
+            var notification = new ShopifyImportNotification(_userNameResolver.GetCurrentUserName())
             {
                 Title = "Import catalog from Shopify",
                 Description = "starting import...."
