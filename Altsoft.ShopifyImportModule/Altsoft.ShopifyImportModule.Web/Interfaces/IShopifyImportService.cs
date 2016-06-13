4aa6b5d0fca84f934ba533464fcaa9508e5fb636
eMazeika <em@virtoway.com>
2016-06-13 13:12:01 +0300

    redundant files removed

--- Altsoft.ShopifyImportModule/Altsoft.ShopifyImportModule.Web/Interfaces/IShopifyImportService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Altsoft.ShopifyImportModule.Web.Models;
-
-namespace Altsoft.ShopifyImportModule.Web.Interfaces
-{
-    public interface IShopifyImportService
-    {
-        ShopifyImportNotification Import(ShopifyImportParams importParams, ShopifyImportNotification notification);
-    }
-}
--- Altsoft.ShopifyImportModule/Altsoft.ShopifyImportModule.Web/Models/AuthenticationModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Altsoft.ShopifyImportModule.Web.Models
-{
-    public class AuthenticationModel
-    {
-        public string ApiKey { get; set; }
-        public string Password { get; set; }
-        public string ShopName { get; set; }
-    }
-}
